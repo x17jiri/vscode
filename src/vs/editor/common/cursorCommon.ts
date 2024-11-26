8cd191d772ca3ebdcd230b2dfd723645cf28f446
Jiri Bobek <jiri.bobek@gmail.com>
2024-11-26 06:45:25 +0100

    Initial

--- src/vs/editor/common/cursorCommon.ts
+++ src/vs/editor/common/cursorCommon.ts
@@ -56,6 +56,7 @@ export class CursorConfiguration {
 	public readonly indentSize: number;
 	public readonly insertSpaces: boolean;
 	public readonly stickyTabStops: boolean;
+	public readonly virtualSpace: boolean;
 	public readonly pageSize: number;
 	public readonly lineHeight: number;
 	public readonly typicalHalfwidthCharacterWidth: number;
@@ -122,6 +123,7 @@ export class CursorConfiguration {
 		this.indentSize = modelOptions.indentSize;
 		this.insertSpaces = modelOptions.insertSpaces;
 		this.stickyTabStops = options.get(EditorOption.stickyTabStops);
+		this.virtualSpace = modelOptions.virtualSpace;
 		this.lineHeight = fontInfo.lineHeight;
 		this.typicalHalfwidthCharacterWidth = fontInfo.typicalHalfwidthCharacterWidth;
 		this.pageSize = Math.max(1, Math.floor(layoutInfo.height / this.lineHeight) - 2);
@@ -235,9 +237,11 @@ export class CursorConfiguration {
 			return minColumn;
 		}
 
-		const maxColumn = model.getLineMaxColumn(lineNumber);
-		if (result > maxColumn) {
-			return maxColumn;
+		if (!this.virtualSpace) {
+			const maxColumn = model.getLineMaxColumn(lineNumber);
+			if (result > maxColumn) {
+				return maxColumn;
+			}
 		}
 
 		return result;
@@ -280,8 +284,9 @@ export class CursorState {
 		const selection = Selection.liftSelection(modelSelection);
 		const modelState = new SingleCursorState(
 			Range.fromPositions(selection.getSelectionStart()),
-			SelectionStartKind.Simple, 0,
-			selection.getPosition(), 0
+			SelectionStartKind.Simple,
+			selection.getPosition(),
+			null
 		);
 		return CursorState.fromModelState(modelState);
 	}
@@ -344,17 +349,15 @@ export class SingleCursorState {
 	constructor(
 		public readonly selectionStart: Range,
 		public readonly selectionStartKind: SelectionStartKind,
-		public readonly selectionStartLeftoverVisibleColumns: number,
 		public readonly position: Position,
-		public readonly leftoverVisibleColumns: number,
+		public readonly columnHint: number | null
 	) {
 		this.selection = SingleCursorState._computeSelection(this.selectionStart, this.position);
 	}
 
 	public equals(other: SingleCursorState) {
 		return (
-			this.selectionStartLeftoverVisibleColumns === other.selectionStartLeftoverVisibleColumns
-			&& this.leftoverVisibleColumns === other.leftoverVisibleColumns
+			this.columnHint === other.columnHint
 			&& this.selectionStartKind === other.selectionStartKind
 			&& this.position.equals(other.position)
 			&& this.selectionStart.equalsRange(other.selectionStart)
@@ -365,24 +368,22 @@ export class SingleCursorState {
 		return (!this.selection.isEmpty() || !this.selectionStart.isEmpty());
 	}
 
-	public move(inSelectionMode: boolean, lineNumber: number, column: number, leftoverVisibleColumns: number): SingleCursorState {
+	public move(inSelectionMode: boolean, lineNumber: number, column: number, columnHint: number | null): SingleCursorState {
 		if (inSelectionMode) {
 			// move just position
 			return new SingleCursorState(
 				this.selectionStart,
 				this.selectionStartKind,
-				this.selectionStartLeftoverVisibleColumns,
 				new Position(lineNumber, column),
-				leftoverVisibleColumns
+				columnHint
 			);
 		} else {
 			// move everything
 			return new SingleCursorState(
 				new Range(lineNumber, column, lineNumber, column),
 				SelectionStartKind.Simple,
-				leftoverVisibleColumns,
 				new Position(lineNumber, column),
-				leftoverVisibleColumns
+				columnHint
 			);
 		}
 	}
